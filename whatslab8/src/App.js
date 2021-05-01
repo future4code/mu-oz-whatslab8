6ae1554d7503b3eec53be3efe42b3a9b38b125d5
Rafael S. da Rosa <rafaelsilveeira@users.noreply.github.com>
2021-04-30 22:26:49 -0300

    estrutura e estilizacao inicial

--- whatslab8/src/App.js
+++ whatslab8/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
+function AdicionarMensagem() {
+  let usuario = document.getElementById('usuario').value
+  let mensagem = document.getElementById('mensagem').value
+  let chat = document.getElementById('chat')
+
+  chat.innerHTML += `${usuario} ${mensagem}\n`;
+
+  console.log({usuario, mensagem})
+}
+
+const Raiz = styled.div `
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  border: 1px solid gray;
+  height: 99vh;
+  width: 600px;
+  margin: 0 auto;
+`
+
+const Chat = styled.div `
+  background-color: gray;
+`
+
+const Inputs = styled.div `
+  display: flex;
+  background-color: orange;
+`
+
+const InputUsuario = styled.input `
+  width: 25%;
+`
+const InputMensagem = styled.input `
+  width: 60%;
+`
+const BotaoEnviar = styled.button `
+  width: 15%;
+`
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Raiz className="raiz">
+        <Chat className="chat" id="chat">
+        </Chat>
+        <Inputs className="inputs">
+          <InputUsuario type="text" placeholder="Usuário" id="usuario"></InputUsuario>
+          <InputMensagem type="text" placeholder="Mensagem" id="mensagem"></InputMensagem>
+          <BotaoEnviar onClick={AdicionarMensagem}>Enviar</BotaoEnviar>
+        </Inputs>
+      </Raiz>
     </div>
   );
 }
